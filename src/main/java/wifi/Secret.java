f869fff90dcdfba1896a6c0aa086d200d68cdbcc
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-07-02 20:10:53 +0900

    Delete Secret.java

--- src/main/java/wifi/Secret.java
+++
@@ -1,8 +0,0 @@
-package wifi;
-
-public class Secret {
-    public static final String KEY = "734242646a68796532375a71666148";
-    public static final String URL = "jdbc:mariadb://127.0.0.1:3306/wifi_db";
-    public static final String DB_USERID = "wifi";
-    public static final String DB_PASSWORD = "hyeonju";
-}
